428bcb97bf8d3defc089bc9a86000bae110ee046
Daxomar <152349184+Daxomar@users.noreply.github.com>
2026-06-17 05:08:38 +0000

    Valid Addition to Vendly v2

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.set('trust proxy', 1);
 
 // CORS configuration - MUST be before other middlewares
 const allowedOrigins = [
-  "https://joy-bundle-frontend.vercel.app",
+  "https://vendly-blush.vercel.app",
   "https://incurrable-wilhelmina-uncolloquially.ngrok-free.dev",
   "https://5e27-154-161-52-97.ngrok-free.app",
   "https://www.joydatabundle.com",
